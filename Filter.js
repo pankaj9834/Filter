8d96458359b8663a8f208c78249b7fe572587405
Pankaj <pankajsonawane9834@gmail.com>
2022-01-27 23:07:38 +0530

    Git filter

---
+++ Filter.js
@@ -0,0 +1,13 @@
+let fruits = ['apple', 'banana', 'grapes', 'mango', 'orange']
+
+/**
+ * Filter array items based on search criteria (query)
+ */
+function filterItems(arr, query) {
+    return arr.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+}
+
+console.log(filterItems(fruits, 'ap')) // ['apple', 'grapes']
+console.log(filterItems(fruits, 'an')) // ['banana', 'mango', 'orange']
